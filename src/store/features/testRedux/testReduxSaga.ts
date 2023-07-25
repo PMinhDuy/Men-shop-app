a393f94ddd78455003f83a4e1c9c48ef6ab04b3f
Son Tran <tran.son@vendor.hcc.com.sg>
2023-07-25 15:35:09 +0700

    deploy web

--- src/store/features/testRedux/testReduxSaga.ts
+++ src/store/features/testRedux/testReduxSaga.ts
@@ -1,12 +1,8 @@
-import axios from "axios";
-import { call, put, takeLatest } from "redux-saga/effects";
+import { put, takeLatest } from "redux-saga/effects";
 import { testReduxAction } from "./testReduxSlice";
-
-const fetchAPI = () => axios.get("http://localhost:3000/testData");
 
 export function* testSaga() {
   try {
-    const response: ReturnType<typeof fetchAPI> = yield call(() => fetchAPI());
     yield put(
       testReduxAction.loadTestReduxDataSucceeded({
         testRedux: {
